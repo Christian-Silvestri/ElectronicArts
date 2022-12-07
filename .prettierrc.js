de3ae70b533a38d9b3dd76b087cbbbc4150236c6
Christian Silvestri <christiansilvestri@live.it>
2022-12-07 20:07:55 +0100

    Clone ElectronicArts WebSite FullResponsive

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 2,
+    semi: true,
+    doubleQuote: true,
+    bracketSpacing: false,
+    bracketSameLine: true,
+    arrowParens: "always",
+    printWidth: 140,
+  };
